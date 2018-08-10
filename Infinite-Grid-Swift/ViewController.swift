56ed498a00cbe8593f77f1e804e8cca9f3905bdb
Dave Poirier <dave@idfusion.com>
2018-08-10 10:54:55 -0400

    Initial project files
    Signed-off-by: Dave Poirier <dave@idfusion.com>

---
+++ Infinite-Grid-Swift/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Infinite-Grid-Swift
+//
+//  Created by Dave Poirier for ID Fusion Software Inc on 2018-08-10.
+//  This is free and unencumbered software released into the public domain.
+//
+//  For countries not supporting unlicensed code:
+//  Copyright (C) 2018 ID Fusion Software Inc. All rights reserved
+//  Distributed under the MIT License: https://opensource.org/licenses/MIT
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
